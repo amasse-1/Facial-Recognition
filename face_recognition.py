01462c941b502e9ae5b9d9f09885219b49bc7c0c
Anthony Masse <46097827+amasse-1@users.noreply.github.com>
2020-03-01 23:29:32 -0500

    Add files via upload

--- face_recognition.py
+++ face_recognition.py
@@ -1,7 +1,29 @@
 import cv2
+import numpy as np
+import os 
 
 #creating Fisherface Recognizer
-face_rec = cv2.face.FisherFaceRecognizer_create(15)
+face_rec = cv2.face.FisherFaceRecognizer_create()
 
-if face_rec.empty():
-    print('Empty data set')
+def read_csv(file1):
+    faces = []
+    faceArray = []
+
+    csv = open(file1)
+    lines = csv.readlines()
+    
+    for line in lines:
+        faces.append(line.strip())
+        for face in faces:
+            #reading images
+            new = cv2.imread(face)
+            #creating gray scale images
+            gray = cv2.cvtColor(new, cv2.COLOR_BGR2GRAY)
+            faceArray.append(gray)
+            return faceArray
+
+faces = read_csv('data.csv')
+names = ['Anthony', 'Jalen', 'Kaylee']
+face_rec.train(src=faces, labels=names)
+
+        
